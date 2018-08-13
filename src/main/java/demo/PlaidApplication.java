a46601e43bde627337f25b9e4ddef3901e5cc7ac
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-08-13 12:00:14 -0700

    added depenancies and connection properties to connect to server.

--- src/main/java/demo/PlaidApplication.java
+++ src/main/java/demo/PlaidApplication.java
@@ -2,17 +2,30 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
-public class PlaidApplication {
+public class PlaidApplication extends SpringBootServletInitializer {
 
     public static void main(String... args) {
         SpringApplication.run(PlaidApplication.class);
     }
 
+    // runs TomCat8 - server
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+        return application.sources(PlaidApplication.class);
+    }
 
 }
+
+// TO RUN LOCALLY:
+// comment out the following to run locally:
+// server info in application, properties.
+// tomcat8 dependency in pom file.
+// extends SpringBoot... and @Override method above.

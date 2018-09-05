2db2f2c36a4c823c3a6c1f204477aeb2f28ce497
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-09-05 15:11:37 -0700

    fixed email service.

--- src/main/java/demo/services/EmailService.java
+++ src/main/java/demo/services/EmailService.java
@@ -18,11 +18,11 @@ public class EmailService {
 
         // Replace sender@example.com with your "From" address.
         // This address must be verified with Amazon SES.
-        static String FROM = "dgc18@live.co.uk";
+        static String FROM = "eemail_address@here";
 
         // Replace recipient@example.com with a "To" address. If your account
         // is still in the sandbox, this address must be verified.
-        static String TO = "dgc18@live.co.uk";
+        static String TO = "email_address@here";
 
         // The configuration set to use for this email. If you do not want to use a
         // configuration set, comment the following variable and the

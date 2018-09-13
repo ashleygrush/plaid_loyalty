11bf69e78b73d818a22c9310740e99d7e2d5a2d6
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-09-13 15:26:58 -0700

    Added Null Pointer Exception Handling.

--- src/main/java/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/demo/exceptions/GlobalExceptionHandler.java
@@ -14,8 +14,29 @@ public class GlobalExceptionHandler {
     /** Provides handling for exceptions throughout this service. */
     @ExceptionHandler(Exception.class)
     @ResponseBody CustomResponseObject handleException(Exception ex, HttpServletRequest req) {
+
+        if (ex instanceof NullPointerException) {
+            return handleNullPointer(ex);
+        } else {
             return defaultErrorMessage(ex);
+        }
     }
+
+    protected CustomResponseObject handleNullPointer(Exception ex) {
+
+        CustomResponseObject response = new CustomResponseObject();
+
+        response.setData("Null Pointer Exception");
+        response.setMessage(ex.getLocalizedMessage());
+        response.setStatusCode(200);
+
+        System.out.println(" - - - - - - - - - - - - ");
+        System.out.println("Print Exception: ");
+        System.out.println(ex.getMessage());
+
+        return response;
+    }
+
 
     // Default Exception Handling Message
     protected CustomResponseObject defaultErrorMessage(Exception ex) {

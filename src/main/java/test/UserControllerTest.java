67146ab747fb09e13c1d8aa3ad0f02ef12ebc2dd
Ashley Almeida <ashleyalmeida@Ashleys-MacBook-Pro.local>
2018-09-27 18:11:46 -0700

    refactored test package for jUnit. Also added User Control Test class.

---
+++ src/main/java/test/UserControllerTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import demo.controller.UserController;
+
+/**
+ * Created by ashleyalmeida on Sep, 2018.
+ */
+
+class UserControllerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateTransactions() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllUsers() {
+        UserController test = new UserController();
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void findByID() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void createUser() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteByID() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateUserByID() {
+    }
+}
